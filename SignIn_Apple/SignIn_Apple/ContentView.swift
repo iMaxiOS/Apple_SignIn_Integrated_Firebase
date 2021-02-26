82ded2d0465993efa1f4b540d533eaa096a15b38
Macostik <y.granchenko@gmail.com>
2021-02-26 11:15:04 +0200

    create project

---
+++ SignIn_Apple/SignIn_Apple/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SignIn_Apple
+//
+//  Created by Maxim Granchenko on 26.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignIn_Apple/SignIn_Apple/SignIn_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignIn_AppleApp.swift
+//  SignIn_Apple
+//
+//  Created by Maxim Granchenko on 26.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SignIn_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
